bf9054c37bcbbce2838aed1d538b940a95a38a69
sk naim <sknaimuddin234@gmail.com>
2021-07-23 16:12:29 +0530

    Update helper.py

--- Helper/helper.py
+++ Helper/helper.py
@@ -1,7 +1,5 @@
 start_text = '''Welcome to Manga DL Bot. Here you can Download all Mangas for Free.
 
-You can Also download Anime for free at @tganimesterambot.
-
 Please read the Instructions carefully before surfing in...
 
 Use /help to know commands and how to use this bot'''
@@ -9,12 +7,18 @@ Use /help to know commands and how to use this bot'''
 help_text = '''List of commands:
 
 /manga <name of Manga you want> : to download any Manga by search
+Example: /manga attack on titan
 
-/nh : </nh <hentai code>
+/nh <hentai code> : to download Hentai
+Example: /nh 350223
 
-/read : <name of Manga you want to read>
+/read <type-manga-name-using-hyphen:chapter_number>
+Example: /read attack-on-titan:1
+
+Note: Sometimes bot cannot find the manga by English name, that case, use the japanese name.
+For example: use "Kimetsu no yaiba" instead of "Demon Slayer"
                                
-The files provided are in multiple qualities to download just open filein chrome
-                               
-To report any Problems, Bugs, Suggestions go to @Animemusicarchive6'''
+The files provided are in multiple qualities to download just open file in chrome
+
+Bot repo: https://github.com/Achu2234/Heroku-Manga-DL-Bot'''
 
